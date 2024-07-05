46581c97696407f7d58ff4ebd72889fd31bdbe3b
XD <Xueying.Deng@imec.be>
2024-07-05 17:20:58 +0200

    feat: pseudo integration

--- fhir-test-services/src/main/java/eu/europa/ec/fhir/Application.java
+++ fhir-test-services/src/main/java/eu/europa/ec/fhir/Application.java
@@ -1,7 +1,12 @@
 package eu.europa.ec.fhir;
 
+import org.imec.ivlab.ehealth.automation.PseudonymGenerator;
+import org.imec.ivlab.ehealth.automation.PseudonymizationApp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.imec.ivlab.ehealth.automation.PseudonymGenerator;
+
+import java.io.File;
 
 /**
  * Entry point to bootstrap the application.
@@ -15,9 +20,10 @@ public class Application {
      * @param args Runtime arguments (none expected).
      */
     public static void main(String[] args) {
+        //new  PseudonymizationApp().run(args);
         SpringApplication.run(Application.class, args);
-    }
 
+    }
 }
 
 
